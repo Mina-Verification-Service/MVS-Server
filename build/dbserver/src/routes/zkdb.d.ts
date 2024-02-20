14d5b03156a94649fd0c2878560a3e64b1c85c9a
JoE11-y <Josephedoh77@gmail.com>
2024-02-20 21:28:42 +0100

    cleanup

--- build/dbserver/src/routes/zkdb.d.ts
+++
@@ -1,4 +0,0 @@
-import type { ParameterizedContext } from "koa";
-export declare function getDBRootHandler(ctx: ParameterizedContext): Promise<void>;
-export declare function getUserHandler(ctx: ParameterizedContext): Promise<void>;
-export declare function addUserHandler(ctx: ParameterizedContext): Promise<void>;
